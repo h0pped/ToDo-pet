a462916b709bcf9abedcf34c56038858ea38a031
awri.l <notawril@gmail.com>
2021-08-26 16:00:27 -0700

    main page

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
+const path = require("path");
+const publicDir = path.join(__dirname, "../public");
+
 const express = require("express");
 const app = express();
+app.use(express.static(publicDir));
+
 const port = 3000;
 require("./db/mongoose");
-
-const UserModel = require("./models/UserModel");
-const FolderModel = require("./models/FolderModel");
-const TaskModel = require("./models/TaskModel");
-const { ObjectId } = require("mongodb");
 
 const userRouter = require("./routes/userRoutes");
 const folderRouter = require("./routes/folderRoutes");
@@ -16,20 +16,8 @@ app.use(express.json());
 app.use(userRouter);
 app.use(folderRouter);
 app.use(taskRouter);
+
 app.get("/", async (req, res) => {
-  const user = await UserModel.findById("612581d5a13da53e58be1cda");
-
-  await user
-    .populate({
-      path: "folders",
-      populate: {
-        path: "tasks",
-      },
-    })
-    .execPopulate();
-
-  console.log(user.folders[0].tasks);
-
   res.send("resr");
 });
 app.listen(port, () => {
