00698aa11212fa6f25e5c6b13bf4d1664e93d761
awri.l <notawril@gmail.com>
2021-08-26 17:18:14 -0700

    static pages

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
 const path = require("path");
+const hbs = require("hbs");
+
 const publicDir = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../views");
+const partialsPath = path.join(__dirname, "../views/partials");
 
 const express = require("express");
 const app = express();
 app.use(express.static(publicDir));
-
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 const port = 3000;
 require("./db/mongoose");
 
 const userRouter = require("./routes/userRoutes");
 const folderRouter = require("./routes/folderRoutes");
 const taskRouter = require("./routes/taskRoutes");
+const staticRouter = require("./routes/staticRoutes");
 
 app.use(express.json());
 app.use(userRouter);
 app.use(folderRouter);
 app.use(taskRouter);
+app.use(staticRouter);
 
-app.get("/", async (req, res) => {
-  res.send("resr");
-});
 app.listen(port, () => {
   console.log("Listening on " + port);
 });
