48f14c37e7ce3f87b049c84b30703376413ab772
awri.l <notawril@gmail.com>
2021-08-27 15:23:01 -0700

    fetch folders from the server

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,77 @@
-let folders = document.getElementsByClassName("folder-title");
+let foldersEl = document.getElementsByClassName("folder-title");
+let foldersContainer = document.querySelector(".folders-container");
+let folders = [];
 
-let i;
+const getTasksByFolder = async (folder) => {
+  return await fetch(`/tasks/byfolder/${folder._id}`).then((data) =>
+    data.json()
+  );
+};
+const getFolders = async () => {
+  const fetchedFolders = await fetch("/folders").then((data) => data.json());
 
-for (i = 0; i < folders.length; i++) {
-  folders[i].addEventListener("click", function () {
-    console.log(this);
-    console.log("hello");
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+  for (let i = 0; i < fetchedFolders.length; i++) {
+    fetchedFolders[i].tasks = await getTasksByFolder(fetchedFolders[i]);
+  }
+  folders = fetchedFolders;
+  console.log(folders);
+  updateUI();
+};
+
+const generateFolder = (data) => {
+  const folder = document.createElement("div");
+
+  folder.classList.add("folder");
+  console.log(data.tasks);
+  let tasks = "";
+
+  data.tasks.forEach((task) => {
+    tasks += `<li><div class="task"><p>${
+      task.title
+    }<span class="date-span">${getConvertedTime(
+      task.updatedAt
+    )}</span></p></div></li>`;
   });
-}
+  console.log(tasks);
+  folder.innerHTML = `
+  <div class="folder-title">
+                  <button class="medium-text link-button">${data.title}</button>
+                </div>
+                <div class="folder-tasks">
+                  <ul>
+                  ${tasks}
+                  </ul>
+                </div>
+  `;
+  foldersContainer.appendChild(folder);
+};
+
+const updateUI = () => {
+  folders.forEach((folder) => {
+    generateFolder(folder);
+  });
+  for (let i = 0; i < foldersEl.length; i++) {
+    foldersEl[i].addEventListener("click", function () {
+      console.log(this);
+      console.log("hello");
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+};
+
+const getConvertedTime = (time) => {
+  const newTime = new Date(time);
+  return (
+    newTime.toLocaleDateString("en-US") +
+    " " +
+    newTime.toLocaleTimeString("en-US")
+  );
+};
+
+getFolders();
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -7,6 +7,13 @@ const router = new express.Router();
 
 // Add new task to the folder
 // id - id of the folder
+router.get("/tasks/byfolder/:id", auth, async (req, res) => {
+  const tasks = await TaskModel.find({
+    folder: req.params.id,
+    owner: req.user._id,
+  });
+  res.send(tasks);
+});
 router.post("/tasks/:id", auth, async (req, res) => {
   const task = new TaskModel({
     ...req.body,
