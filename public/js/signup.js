97410a8629c26d8f0ab6ba066857f9312ac3c1bb
awri.l <notawril@gmail.com>
2021-08-26 17:40:50 -0700

    sign up works on the front-end

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("form");
 const email = document.querySelector("#email_input");
+const nameel = document.querySelector("#name_input");
 const password = document.querySelector("#password_input");
 const icon = document.querySelector("lord-icon");
 
@@ -9,23 +10,24 @@ form.addEventListener("submit", async (e) => {
   e.preventDefault();
   icon.setAttribute("trigger", "loop");
   data = {
+    name: nameel.value,
     email: email.value,
     password: password.value,
   };
   try {
-    const response = await fetch("/users/login", {
+    const response = await fetch("/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-    if ((response.status = 200)) {
+    if ((response.status = 201)) {
       const json = await response.json();
 
       if (json.user && json.token) {
         localStorage.setItem("token", json.token);
-        window.location.href = "/sup";
+        window.location.href = "/supchik";
       }
     }
   } catch (err) {
