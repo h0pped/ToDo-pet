7e02bdc108da35680fe968f5c949e088ece2f2b7
h0pped <notawril@gmail.com>
2021-08-31 14:18:20 +0300

    logout functionality

--- src/routes/staticRoutes.js
+++ src/routes/staticRoutes.js
@@ -4,6 +4,9 @@ const auth = require("../middlewares/auth");
 const router = new express.Router();
 
 router.get("", (req, res) => {
+  if (req.cookies.authToken) {
+    return res.redirect("/main");
+  }
   res.render("index");
 });
 router.get("/signin", (req, res) => {
@@ -19,6 +22,10 @@ router.get("/signup", (req, res) => {
   }
   // console.log(req.cookies)
   return res.render("signup");
+});
+router.get("/logout", auth, (req, res) => {
+  delete res.clearCookie("authToken");
+  return res.redirect("/");
 });
 router.get("/main", auth, (req, res) => {
   res.render("main", { username: req.user.name });
